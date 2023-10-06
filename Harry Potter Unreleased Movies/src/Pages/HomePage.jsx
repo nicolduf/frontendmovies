8dbcea5823368e010d6c5b0e35b49e645b1607ea
“Nicole <itsnicoleduffy@gmail.com>
2023-10-06 19:08:34 +0200

    friday commit

--- Harry Potter Unreleased Movies/src/Pages/HomePage.jsx
+++ Harry Potter Unreleased Movies/src/Pages/HomePage.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 
 function HomePage() {
-    return (
-        <div>
-            <h1>Harry Potter - The Unreleased Movies you never knew you needed!</h1>
-        </div>
-    );
-}
+   
+  }
+  
+  export default HomePage;
 
-export default HomePage;
